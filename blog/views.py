7e15998b284e99563f9664c009031b273086901e
smajev <smajev@gmail.com>
2021-10-28 15:50:20 +0200

    views2

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,16 @@ def post_list(request):
     return render(
         request, 'blog/post/list.html', {'posts': posts}  
         )
+
+def post_detail(request, year, moth, day, post):
+    post = get_object_or_404(Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=moth,
+        publish__day=day
+    )
+    return render(
+        request, 'blog/post/detail.html',
+        {'post': post}
+    )
