25318b6ac85f485554b07f7eb2deb7145e2f846d
smajev <smajev@gmail.com>
2021-10-28 16:46:25 +0200

    2 views

--- blog/views.py
+++ blog/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.published.all()
     return render(
         request, 'blog/post/list.html', {'posts': posts}  
         )
 
-def post_detail(request, year, moth, day, post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
         slug=post,
         status='published',
         publish__year=year,
-        publish__month=moth,
+        publish__month=month,
         publish__day=day
     )
     return render(
